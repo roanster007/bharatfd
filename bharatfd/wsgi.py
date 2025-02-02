d9db02ea98fa6da23750fbf5ea9645733c6c2387
Rohan Gudimetla <rohan.gudimetla07@gmail.com>
2025-02-02 22:22:29 +0530

    cache: Add support to cache most frequently used data.
    This commit introduces caching, which caches the
    English translations of FAQs, since they are the most
    frequently used translations.

--- bharatfd/wsgi.py
+++ bharatfd/wsgi.py
@@ -10,7 +10,11 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from faqs.lib import initialize_services
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bharatfd.settings")
 
 application = get_wsgi_application()
+
+# Intitialize the cache services.
+initialize_services()
--- faqs/lib.py
+++ faqs/lib.py
@@ -1,12 +1,58 @@
+import redis
+import json
+
 from googletrans import Translator
 
 from asgiref.sync import sync_to_async
+from django.conf import settings
 from django.db import transaction
 from django.http import JsonResponse
 from faqs.models import FAQ, FAQTranslation, Language
 
-translator = Translator()
 supported_langs = [lang.value for lang in Language]
+
+# Redis Cache Services
+redis_client = None
+faq_cache_key = "faqs_en"
+
+# Google Translate Service
+translator = None
+
+
+# Initializes services when the server is started
+def initialize_services():
+    # Initialize `googletrans` translator
+    global translator
+    translator = Translator()
+
+    # Initialize redis client.
+    initialize_redis_client()
+    
+
+
+def initialize_redis_client():
+    global redis_client
+    global faq_cache_key
+
+    redis_client = redis.StrictRedis(
+        host=settings.REDIS_HOST,
+        port=settings.REDIS_PORT,
+        db=settings.REDIS_DB,
+        decode_responses=True,
+    )
+
+    # Invalidate all the old cache
+    redis_client.delete(faq_cache_key)
+
+    # We cache the FAQs in English language
+    # since it is the most frequently used
+    # one.
+    faqs = FAQTranslation.objects.filter(lang=Language.EN)
+    faq_data = [faq.to_dict() for faq in faqs]
+
+    for faq in faq_data:
+        redis_client.rpush(faq_cache_key, json.dumps(faq))
+
 
 
 # For each FAQ, we create a new row in the FAQ table
@@ -47,9 +93,16 @@ async def create_translations(faq, question, answer):
 
         translations.append(faq_translation)
 
+        # We want to cache english translation for future
+        # use.
+        if lang == Language.EN:
+            en_translated = faq_translation.to_dict()
+
     await sync_to_async(FAQTranslation.objects.bulk_create, thread_sensitive=True)(
         translations
     )
+
+    redis_client.rpush(faq_cache_key, json.dumps(en_translated))
 
 
 def delete_faq(faq_id):
@@ -60,12 +113,27 @@ def delete_faq(faq_id):
     # Then delete the reference to the FAQ
     FAQ.objects.filter(id=faq_id).delete()
 
+    # Remove that faq from the cache
+    faq_data = [json.loads(item) for item in redis_client.lrange(faq_cache_key, 0, -1)]
+    redis_client.delete(faq_cache_key)
+
+    for faq in faq_data:
+        if faq.get("id") == faq_id:
+            continue
+        
+        redis_client.rpush(faq_cache_key, json.dumps(faq))
+
 
 def maybe_get_faq_data(lang):
     if lang not in supported_langs:
         return
     
-    faqs = FAQTranslation.objects.filter(lang=lang)
-    faq_data = [faq.to_dict() for faq in faqs]
+    # If the requested language is `English`, use the results
+    # from the cache.
+    if lang == Language.EN:
+        faq_data = [json.loads(item) for item in redis_client.lrange(faq_cache_key, 0, -1)]
+    else:
+        faqs = FAQTranslation.objects.filter(lang=lang)
+        faq_data = [faq.to_dict() for faq in faqs]
 
     return faq_data
